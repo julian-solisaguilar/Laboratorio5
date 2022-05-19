7e8a15d6cc75a4edac6661631159f4e722795ba5
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-05-19 14:18:49 -0600

    Implementacion del controlador, handler y model de Paises, ademas de el index con sus respectivos cambios en el layout.

---
+++ Laboratorio5/Controllers/PaisesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorio5.Handlers;
+
+namespace Laboratorio5.Controllers
+{
+  public class PaisesController : Controller
+  {
+    public IActionResult Index()
+    {
+      PaisesHandler paisesHandler = new PaisesHandler();
+      var paises = paisesHandler.ObtenerPaises();
+      ViewBag.MainTitle = "Lista de Paises";
+      return View(paises);
+    }
+  }
+}
---
+++ Laboratorio5/Queries_Utilizadas.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE Pais(
+	Id INTEGER PRIMARY KEY IDENTITY,
+	Nombre VARCHAR(200) NOT NULL,
+	Idioma VARCHAR(200) NOT NULL,
+	Continente NVARCHAR(100) NOT NULL
+)
+
+INSERT INTO [dbo].[Pais] ([Nombre],[Idioma] ,[Continente])
+VALUES('Costa Rica', 'Español', 'América'),
+('Alemania', 'Alemán', 'Europa'),
+('Canadá', 'Inglés/Frances', 'América'),
+('Japón', 'Japonés', 'Asia'),
+('España', 'Español', 'Europa')
+
+SELECT * 
+FROM Pais
